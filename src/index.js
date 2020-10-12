9a62e8906fd1846aed1693fb7737441ba9a7b5b8
mrStatic23 <mrstatic23@yandex.ru>
2020-10-12 23:40:46 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = bracketsConfig.map(item => item.join(''));
+
+    for (let i = 0; i < brackets.length; i++) {
+        const bracket = brackets[i];
+
+        if(str.includes(bracket)) {
+            str = str.split(bracket).join('')
+            i = -1
+        }
+        
+    }
+      
+    return str.length === 0 ? true : false
 }
